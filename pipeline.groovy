d792bb2cf164fb1348a5c2dc703a7a3baab61488
SupunTJ <130586987+SupunJayaweera@users.noreply.github.com>
2024-04-22 18:10:48 +0530

    adding pipeline.groovy file

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,33 +1,29 @@
 pipeline {
-  agent any
-  triggers{
-    githubPush()
-  }
-  stages {
-    stage('build'){
-      steps {
-        sh 'docker build -t thetharz/3996-node-app-image .'
-      }
-    }
-    stage('run'){
-      steps{
-        sh 'docker run -d -p 5000:3000 thetharz/3996-node-app-image'
-      }
-    }
-    stage('final'){
+    agent any
+    stages {
+        stage ('SCM checkout'){
+            steps{
+                retry(3){
+                    checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/SupunTJ/3998-jayaweera']])
+                }
+            }
+      
+        }
+        stage('build docker'){
+            steps {
+                sh 'docker build -t Supun3998/3998-jayaweera .'
+            }
+        
+        }
+         stage('run'){
+            steps{
+                sh 'docker run -d -p 5000:3000 Supun3998/3998-jayaweera'
+         }
+         }
+         stage('show running containners'){
       steps{
         sh 'docker ps'
       }
     }
-    stage('push'){
-      steps{
-        script{
-          withCredentials([string(credentialsId: 'docker-pwd', variable: 'docker')]) {
-    sh 'docker login -u thetharz -p ${docker}'
-  }
-    sh 'docker push thetharz/3996-node-app-image'
-        }
-      }
     }
-  }
 }
